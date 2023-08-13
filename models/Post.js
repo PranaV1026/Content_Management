c4dee81ed94cc1d0f5d853fa3683dae1036ed1f3
PranaV1026 <shimpipranav78@gmail.com>
2023-08-13 23:06:41 +0530

    Uploading Files to Github

---
+++ models/Post.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const {Schema,model} = mongoose;
+
+const PostSchema = new Schema({
+  title:String,
+  summary:String,
+  content:String,
+  cover:String,
+  author:{type:Schema.Types.ObjectId, ref:'User'},
+}, {
+  timestamps: true,
+});
+export default model("Post",PostSchema);
